60b06851a0a2903a6a479b3b3c7f79664f293f3d
Stephen <stewxy13@gmail.com>
2024-04-05 18:00:57 +1300

    Created MySQL database and fixed errors in code. Connection to database established successfully.

--- src/main/java/packages/DbManager.java
+++ src/main/java/packages/DbManager.java
@@ -7,15 +7,13 @@ import java.sql.SQLException;
 public class DbManager {
 	public Connection getConnection(){
 		try {
-			Class.forName("com.mysql.jbdc.Driver");
-			Connection conn = DriverManager.getConnection("jbdc:mysql://localhost:3306/location", "root", "admin");
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaprojectdb", "root", "admin");
 			return conn;
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
