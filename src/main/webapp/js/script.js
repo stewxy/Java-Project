89c615b4ebdaef766ab5b98bf487bb1d2b6722e9
Stephen <stewxy13@gmail.com>
2024-05-05 21:34:33 +1200

    added temporary test text and canvas data capture

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,5 +1,7 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+canvas.wwidth = window.innerWidth;
+canvas.height = window.innerHeight;
 
 let mouse = {
 	x: null,
@@ -8,6 +10,15 @@ let mouse = {
 }
 
 window.addEventListener("mousemove", function(event){
-	mouse.x = event.x
-	mouse.y = event.y
+	mouse.x = event.x;
+	mouse.y = event.y;
 })
+
+ctx.fillStyle = "white";
+ctx.font = "30px Verdana";
+ctx.fillText("A", 0, 30);
+
+//ctx.strokeStyle = "white";
+//ctx.strokeRect(0, 0, 100, 100);
+
+let data = ctx.getImageData(0, 0, 100, 100);
