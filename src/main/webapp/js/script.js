04f389b0ab6378091be90728b91178f79abff110
Stephen <stewxy13@gmail.com>
2024-05-07 21:17:52 +1200

    added particle class and init method to store particles in particleArray

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -9,10 +9,12 @@ let mouse = {
 	radius: 100
 }
 
+let particleArray = [];
+
 window.addEventListener("mousemove", function(event){
 	mouse.x = event.x;
 	mouse.y = event.y;
-})
+});
 
 ctx.fillStyle = "white";
 ctx.font = "30px Verdana";
@@ -22,3 +24,29 @@ ctx.fillText("A", 0, 30);
 //ctx.strokeRect(0, 0, 100, 100);
 
 let data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle{
+	constructor(x, y){
+	this.x = x;
+	this.y = y
+	this.size = 3;
+	this.baseX = this.x;
+	this.baseY = this.y;
+	this.density = (Math.random() * 30) + 1;
+	}
+	draw(){
+		ctx.fillStyle = "white";
+		ctx.beginPath();
+		ctx.ard(this.x, this.y, this.size, 0, Math.PI * 2);
+		ctx.closePath();
+		ctx.fill();
+	}
+}
+
+function init(){
+	particleArray= [];
+	particleArray.push(new Particle(50, 50));
+	particleArray.push(new Particle(80, 50));
+}
+init();
+console.log(particleArray);
