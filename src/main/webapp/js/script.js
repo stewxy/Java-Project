5cbcceab2330d6cf04c516a984e7c4f73cab672a
Stephen <stewxy13@gmail.com>
2024-05-14 14:36:07 +1200

    Modified init function so that particles form the shape of text

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -19,11 +19,12 @@ window.addEventListener("mousemove", function(event){
 ctx.fillStyle = "white";
 ctx.font = "30px Verdana";
 ctx.fillText("A", 0, 30);
+let textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
 //ctx.strokeStyle = "white";
 //ctx.strokeRect(0, 0, 100, 100);
 
-let data = ctx.getImageData(0, 0, 100, 100);
+
 
 class Particle{
 	constructor(x, y){
@@ -71,10 +72,15 @@ class Particle{
 
 function init(){
 	particleArray= [];
-	for(let i = 0; i < 1000; i++){
-		let x = Math.random() * canvas.width;
-		let y = Math.random() * canvas.width;
-		particleArray.push(new Particle(x, y));
+	for(let y = 0, y2 = textCoordinates.height; y < y2; y++){
+		for(let x = 0, x2 = textCoordinates.width; x < x2; x++){
+			if(textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128){
+				let positionX = x;
+				let positionY = y;
+				particleArray.push(new Particle(positionX * 10, positionY * 10));
+			}
+		}
+		
 	}
 }
 init();
